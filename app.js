45f33b7b9302eb20263f65d8ed83e73f66518dc0
washington fernandes <washingtonluis@ymail.com>
2023-11-13 17:14:31 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementById('login-form').addEventListener('submit', (event) => {
+    event.preventDefault()
+
+const inpuEmail = document.querySelector('#input-email').value
+const inpuPassword = document.querySelector('#input-password').value
+console.log(inpuEmail,inpuPassword)
+})
+
+
